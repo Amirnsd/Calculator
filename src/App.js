cef940450d430bef508061ad7bde415ef91e512c
Amirnsd <amirhosein279279@gmail.com>
2023-06-10 23:03:42 -0400

    New project to push to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [inputs, setInputs]=useState("");
+
+
+    const handleClick = (e) => {
+        
+        setInputs(inputs.concat(e.target.value));
+    }
+
+
+    const handleClear = (e) => {
+             
+        setInputs("");
+    }
+
+
+    const DeleteElement = (e) =>{
+
+        setInputs(inputs.slice(0,inputs.length-1));
+    }
+
+
+    const handleEqual = () => {
+        
+        try {
+            setInputs(eval(inputs).toString());
+
+        } catch(err) {
+            setInputs("Error");
+        }
+       
+        
+    }
+
+    return (
+
+        <div className="App">  
+
+    <div className="calculator-wrapper" >
+
+        <form>
+            <input type="text" value={inputs} />
+        </form>
+      
+        <div className="Row">
+            <button onClick={handleClear} id="clear">Clear</button>
+            <button onClick={DeleteElement}>C</button>
+            <button value ="/" onClick={handleClick}>&divide;</button>
+            <button value ="7" onClick={handleClick} >7</button>
+            <button value ="8" onClick={handleClick}>8</button>
+            <button value ="9" onClick={handleClick}>9</button>
+            <button value ="*" onClick={handleClick}>x</button>
+            <button value ="4" onClick={handleClick}>4</button>
+            <button value ="5" onClick={handleClick}>5</button>
+            <button value ="6" onClick={handleClick}>6</button>
+            <button value ="-" onClick={handleClick}>-</button>
+            <button value ="1" onClick={handleClick}>1</button>
+            <button value ="2" onClick={handleClick}>2</button>
+            <button value ="3" onClick={handleClick}>3</button>
+            <button value ="+" onClick={handleClick}>+</button>
+            <button value ="0" onClick={handleClick}>0</button>
+            <button value ="." onClick={handleClick}>.</button>
+            <button value = "=" onClick={handleEqual} id="equal">=</button>
+        </div>
+
+        </div>
+
+        </div>
+    )
 }
+
+
+
 
 export default App;
